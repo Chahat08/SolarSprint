abba0ea7c5f0f7d25c9dd36dd3d0cef3512c91b7
Chahat Kalsi <chahat.ck88@gmail.com>
2021-01-08 23:19:48 +0530

    made the basic website structure

--- solarsprint/views.py
+++ solarsprint/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'solarsprint/game.html', {})
+
+def login(request):
+    return render(request, 'solarsprint/login.html', {})
